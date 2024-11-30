9d70d8c98cf80bf33721ad234e190248ef14bf59
abi <abicherayakkattu2012@gmail.com>
2024-11-30 09:43:33 +0530

    add dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "com.abi.flagchallenge"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.abi.flagchallenge"
@@ -66,4 +68,19 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Hilt
+    implementation (libs.hilt.android)
+    kapt (libs.hilt.compiler)
+    kapt (libs.androidx.hilt.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Coil Image
+    implementation(libs.coil.compose)
+
+    //Data Store
+    implementation(libs.androidx.datastore.preferences)
 }
--- app/src/main/java/com/abi/flagchallenge/activity/MainActivity.kt
+++ app/src/main/java/com/abi/flagchallenge/activity/MainActivity.kt
@@ -1,16 +1,9 @@
-package com.abi.flagchallenge
+package com.abi.flagchallenge.activity
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.abi.flagchallenge.ui.theme.FlagChallengeTheme
 
 class MainActivity : ComponentActivity() {
